6f782768bc856c36e5c35039513d3e59f04d1cb0
AJAY SHARMA <ajaysharma2201@gmail.com>
2019-11-05 02:30:01 +0530

    update-2018.11.5.2

---
+++ Tree/tree_count_number_of_nodes.cc
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define endl "\n"
+
+class Node
+{
+public:
+	int data;
+	Node* left;
+	Node* right;
+	Node(const int d){
+		data = d;
+		left = NULL;
+		right = NULL;
+	}
+	~Node();
+};
+
+#define pni pair<Node*,int>
+// This implementation does'nt take the root pointer as the input.
+// and at the end of the processing it returns the address of new root 
+// node.
+
+Node* build(){
+	int data;
+	cin >> data;
+	if(data==-1){
+		return NULL;
+	}
+	Node* root = new Node(data);
+	root->left = build();
+	root->right = build();
+	return root;
+}
+
+// This implementation takes the root pointer as the input and at 
+// the end of the processing it returns the address of same root 
+// node that it gets from the user.
+
+Node* buildTree(Node* root){
+	int data;
+	cin >> data;
+	if(data==-1) return NULL;
+	root = new Node(data);
+	root->left = buildTree(root->left);
+	root->right = buildTree(root->right);
+	return root;
+}
+
+int countNode(Node* root){
+	return (root==NULL)
+		? 0
+		: 1+countNode(root->left)+countNode(root->right);	
+}
+
+
+int main(){
+	#ifndef ONLINE_JUGDE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	freopen("error.txt","w",stderr);
+	#endif	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	Node* root = NULL;
+	root = build();
+	cout<<countNode(root)<<endl;
+	return 0;
+}
+
+
+
+// TestCases.
+
+// Input : 
+
+// 3 4 5 6 -1 -1 7 -1 -1 8 -1 -1 9 -1 10 11 -1 -1 12 13 -1 -1 14 -1 -1
+// 8 10 1 -1 -1 -1 3 6 9 -1 -1 7 -1 -1 14 13 -1 -1
+
+// Output :  
+
+// 12
+// 9 
+
