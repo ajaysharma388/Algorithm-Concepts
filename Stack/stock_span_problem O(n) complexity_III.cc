33fec3825783285e16c5983c0215bacbcc862518
AJAY SHARMA <ajaysharma2201@gmail.com>
2019-11-12 04:23:34 +0530

    update-2019.11.12.1

--- Stack/stock_span_problem O(n) complexity_III.cc
+++ Stack/stock_span_problem O(n) complexity_III.cc
@@ -10,11 +10,21 @@ using namespace std;
 void stockSpan(int n,int *a){
 	stack<int> s;
 	int *res = new int[n];
-	// code here.
+	for(int i=0;i<n;++i){
+		while(!s.empty() && a[i]>=a[s.top()]){
+			s.pop();
+		}
+		if(s.empty()){
+			res[i] = i+1;
+		}else{
+			res[i] = i-s.top();
+		}
+		s.push(i);
+	}
 	for(int i=0;i<n;++i){
 		cout<<res[i]<<" ";
 	}
-	cout<<"END";
+	cout<<"END"<<endl;
 }
 
 int main(){
@@ -25,13 +35,17 @@ int main(){
 	#endif
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int n;
-	cin >> n;
-	int *a = new int[n];
-	for(int i=0;i<n;++i){
-		cin >> a[i];
+	int testCase;
+	cin >> testCase;
+	while(testCase--){
+		int n;
+		cin >> n;
+		int *a = new int[n];
+		for(int i=0;i<n;++i){
+			cin >> a[i];
+		}
+		stockSpan(n,a);
+		delete []a;
 	}
-	stockSpan(n,a);
-	delete []a;
 	return 0;
 }
