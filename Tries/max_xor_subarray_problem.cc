d9b0b6ad1fb0ecaa495040965a54428da75d959b
Ajay Sharma <ajaysharma388>
2020-01-06 22:27:45 +0530

    update.2020.01.06.2

--- Tries/max_xor_subarray_problem.cc
+++ Tries/max_xor_subarray_problem.cc
@@ -13,7 +13,7 @@ public:
 	trienode*right;
 };
 
-void insert(int n,trienode*head){
+void insert(trienode*head,int n){
 	trienode*curr = head;
 	// itarting from msb towards lsb.
 	for(int i = 31; i >= 0; --i){
@@ -71,6 +71,27 @@ int findMaxXorPair(trienode*head,int*arr,int element){
 	return max_xor;
 }
 
+int subarray_with_max_xor(int*arr,int n){
+	int curr_xor = 0;
+	for(int i = 0; i < n; ++i){
+		arr[i] = arr[i] ^ curr_xor;
+		curr_xor = arr[i];
+	}	
+	for (int i = 0; i < n; ++i)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << " -> " ;
+	trienode *head = new trienode();
+	int result = INT_MIN;
+	for(int i = 0; i < n; ++i){
+		insert(head,arr[i]);
+		int x = findMaxXorPair(head,arr,arr[i]);
+		result = max(result,x);
+	} 
+	return result;
+}
+
 
 int main(){
 	#ifndef ONLINE_JUGDE
@@ -80,29 +101,39 @@ int main(){
 	#endif	
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int n;
-	cin >> n;
-	int *arr = new int[n]();
-	for(int i = 0; i < n; ++i){
-		cin >> arr[i];
-	}	
-	trienode *head = new trienode();
-	int result = INT_MIN;
-	for(int i = 0; i < n; ++i){
-		insert(arr[i],head);
-		int x = findMaxXorPair(head,arr,arr[i]);
-		result = max(result,x);
-	} 
-	cout << result << endl;
-	delete []arr;
+	int testcases;
+	cin >> testcases;
+	while(testcases--){
+		int n;
+		cin >> n;
+		int *arr = new int[n]();
+		for(int i = 0; i < n; ++i){
+			cin >> arr[i];
+		}
+		cout << subarray_with_max_xor(arr,n) << endl;
+		delete []arr;
+	}
 	return 0;
 }
 
-// Sample Input : 
+// Sample Input :
 
+// 5
+// 4
+// 1 2 3 4
+// 6
+// 8 1 2 12 7 6
 // 6
 // 3 10 5 25 8 2
+// 2
+// 4 6
+// 13
+// 2 5 3 7 7 7 0 5 3 8 2 6 4
 
-// Sample Output : 
+// Sample Output :
 
-// 28
+// 1 3 0 4  -> 7
+// 8 9 11 7 0 6  -> 15
+// 3 9 12 21 29 31  -> 30
+// 4 2  -> 6
+// 2 7 4 3 4 3 3 6 5 13 15 9 13  -> 15
