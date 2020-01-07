97758d32e652886edcb45bc6160647b72389a58f
Ajay Sharma <ajaysharma388>
2020-01-08 01:12:07 +0530

    update.2020.01.08.2

---
+++ Greedy Algorithm/maximum_circles_greedy_algorithm.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define endl "\n"
+
+bool compare(pair<int,int> coordsA,pair<int,int> coordsB){
+	return coordsA.second < coordsB.second; 
+}
+
+int main(){
+	#ifndef ONLINE_JUGDE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	freopen("error.txt","w",stderr);
+	#endif	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	vector<pair<int,int>> circles;
+	for(int i = 0; i < n; ++i){
+		int x,r;
+		cin >> x >> r;
+		pair<int,int> coords = {x-r,x+r};
+		circles.push_back(coords);
+	}	
+	sort(circles.begin(), circles.end(), compare);
+	int endCords = -1;
+	int count = 0;
+	for(auto i = 0; i < n; ++i){
+		if(endCords == -1 or endCords <= circles[i].first){
+			endCords = circles[i].second;
+			count += 1;
+		}
+ 	}
+ 	cout << (n-count) << endl;
+	return 0;
+}
