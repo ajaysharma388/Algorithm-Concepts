e3caf27474636496414c69114560c5b2e0584d5d
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2025-08-30 01:54:34 +0530

    Update Problem_4_1_1.cc

--- Topic Wise Problems/4.1 Arrays/Problem_4_1_1.cc
+++ Topic Wise Problems/4.1 Arrays/Problem_4_1_1.cc
@@ -1,17 +1,30 @@
-// 1.  Find a pair in an array of size 'n', whose sum is X
-
 #include <bits/stdc++.h>
-using namespace std;   
+using namespace std;
+
+void fastio(bool read = false) {
+    if(read) {
+        #ifndef ONLINE_JUGDE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+        freopen("error.txt","w",stderr);
+        #endif  
+    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return;
+}
 
 int main() {
+    fastio(true);
     int n, x;
     cin >> n >> x;
     int arr[n]; 
-
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) 
+    {
         cin >> arr[i];
     }   
-    sort(arr, arr + n);
+    
     int left = 0, right = n - 1;
     bool found = false;     
     while(left < right) {
@@ -33,3 +46,19 @@ int main() {
     }
     return 0;
 }   
+
+
+/*
+
+Sample Testcase:
+
+10 18
+1 2 3 4 5 6 7 8 9 10
+
+Sample Output:
+
+Pair found: 8, 10
+
+
+
+*/
