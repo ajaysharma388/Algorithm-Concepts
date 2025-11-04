64cff3d28ccbe9351b4d4dd2c5ccb6b785a36654
AJAY SHARMA <ajaysharma388@users.noreply.github.com>
2025-11-04 16:35:13 +0530

    Adding the problem 133 from LC

---
+++ Blind_150/Python/Graph/LeetCode_133-CloneGraph.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        node_map = dict()
+        def dfs(root: Optional['None']) -> Node:
+            if root == None: return None
+            if root.val in node_map: return node_map[root.val]
+            node_map[root.val] = Node(root.val)
+            for neighbor in root.neighbors:
+                node_map[root.val].neighbors.append(dfs(neighbor))
+
+            return node_map[root.val]
+        return dfs(node)
