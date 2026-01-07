f0537414e03279ec7d58065973735d5017476beb
AJAY SHARMA <ajaysharma388@users.noreply.github.com>
2026-01-07 15:45:37 +0530

    Adding the recusion problems

---
+++ Topic Wise Problems/9 Recursion/Problem_9_7.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fastio(bool read = false) 
+{
+    if(read) 
+    {
+        #ifndef ONLINE_JUGDE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+        freopen("error.txt","w",stderr);
+        #endif  
+    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return;
+}
+
+class Solution 
+{
+public:
+    void solve()
+    {
+       cout << "Ajay Sharma"
+    }
+};
+
+int main() 
+{
+    fastio(true);
+    int tc;
+    cin >> tc;
+    for(int t = 1; t <= tc; ++t) 
+    {    
+        Solution *s = new Solution();
+        cout << "TestCase #" << t << " : ";
+        s->solve();        
+    }
+    return 0;
+}
+
+/*
+Sample Input:
+
+Sample Output:
+
+*/
